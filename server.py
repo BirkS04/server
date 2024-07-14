6dfd6d5a2e14ed539150f2c40bf600875dbd758e
Birk Scheben <birkscheben@MacBook-Air-von-Birk.local>
2024-07-14 19:52:47 +0200

    alles für den server

---
+++ server.py
@@ -0,0 +1,29 @@
+import templates
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+if __name__== "__main__":
+    app.run(debug=True)    
+
+
+# @app.route("/")
+# def hello_world():
+#     return render_template("index.html")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/<string:page_name>")
+def html_page(page_name):
+    return render_template(page_name)
+
+# def routing():
+#     for i in templates:
+#         @app.route(f'/{i}')
+#         def hi():
+#             return render_template(f'/{i}')
+    
+# routing()
